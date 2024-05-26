663c43e47c1920a06b180177a13c971a24688fdf
arun-reddy902 <vanteruarunkumar@gmail.com>
2024-05-26 23:51:58 +1000

    changed model

--- models/model.js
+++ models/model.js
@@ -15,6 +15,30 @@ connect
 	.catch(() => {
 		console.log("Database can not be connected");
 	});
+//Creating the Schema(structure for database)
+const SignupSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
 
-	module.exports = { collection, collection2 };
+const ShopSchema = new mongoose.Schema({
+	shopname: {
+		type: String,
+		required: true,
+	},
+});
+
+//Creating the collections(users and shop) for the databases
+const collection = new mongoose.model("users", SignupSchema);
+const collection2 = new mongoose.model("shop", ShopSchema);
 module.exports = { collection, collection2 };
