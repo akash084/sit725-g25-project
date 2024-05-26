59d292baf2765245d1e003ff1a0140dd11aa2485
Santhush999 <164192235+Santhush999@users.noreply.github.com>
2024-05-26 23:21:36 +1000

    changed model.js and script.js

--- models/model.js
+++ models/model.js
@@ -1,48 +1,8 @@
-// // const express = require("express");
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// const uri =
-// 	"mongodb+srv://akashbaniya084:DM5lOlHA5eEl94Bd@cluster0.4edcxj4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// const client = new MongoClient(uri, {
-// 	serverApi: {
-// 		version: ServerApiVersion.v1,
-// 		strict: true,
-// 		deprecationErrors: true,
-// 	},
-// });
-
-// async function run() {
-// 	try {
-// 		await client.connect();
-// 		await client.db("admin").command({ ping: 1 });
-// 		console.log(
-// 			"Pinged your deployment. You successfully connected to MongoDB!"
-// 		);
-// 	} finally {
-// 		await client.close();
-// 	}
-// }
-
-// async function postCard(card) {
-// 	await client.connect();
-// 	let collection = await client.db().collection("Cat");
-// 	return collection.insertOne(card);
-// }
-
-// async function getAllCards() {
-// 	await client.connect();
-// 	let collection = await client.db().collection("Cat");
-// 	return collection.find().toArray();
-// }
-
-// module.exports = {
-// 	run,
-// 	postCard,
-// 	getAllCards,
-// 	client,
-// };
-
+//Setting up the mongodb
+//Importing mongoose
 const mongoose = require("mongoose");
+
+//connecting to the database
 const connect = mongoose.connect(
 	"mongodb+srv://akashbaniya084:vzrbwXMePKZ2N6vE@cluster0.tfogrds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 );
@@ -56,33 +16,5 @@ connect
 		console.log("Database can not be connected");
 	});
 
-// Create a schema
-const LoginSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-});
-const SignupSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-});
-
-// collection part
-const collection = new mongoose.model("users", SignupSchema);
-
-module.exports = collection;
+	module.exports = { collection, collection2 };
+module.exports = { collection, collection2 };
--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,27 @@ function getShops() {
 	$.get("/home/data", (response) => {
 		//Storing the obtained data from get request
 		responseList = response.data;
-});
+   
+		//Run foreach loop to iterate through each shops and display shopname and its id
+		responseList.forEach((element) =>
+			//Inserting the Adding the HTML code inside div tag having id = shopContainer
+			$("#shopContainer").append(
+				"<form action='/home' class='updateForm' method='POST'>" +
+					"<li style='display:flex; width:100%; align-items: center;>" +
+					"<h4 style='padding:10px'>" +
+					element.shopname +
+					"</h4>" +
+					"<h4 style='padding:10px' id='shopid'>" +
+					element._id +
+					"<h4>" +
+					"<input type='hidden' id='inputid' name='inputid' required/>" +
+					"<Button type='button' style='padding:10px' id='updateShop' style='margine:0;padding:0; height:40px'> Update </Button>" +
+					"<Button type='button' style='padding:10px' id='deleteShop' style='margine:0;padding:0; height:40px'> Delete </Button>" +
+					"</li>" +
+					"</form>"
+			)
+		);
+	});
 }
 
 //When the app is started in the browser the below js function runs
